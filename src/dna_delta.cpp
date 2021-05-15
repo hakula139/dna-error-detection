70d5bff1485dc4bf2c863ff4baf1f9e12719d21d
Hakula Chen <i@hakula.xyz>
2021-05-15 15:22:55 +0800

    refactor: stringify range

--- src/dna_delta.cpp
+++ src/dna_delta.cpp
@@ -17,25 +17,34 @@ string Stringify(const string& key, const Range& range) {
   return key + " " + to_string(range.start_) + " " + to_string(range.end_);
 }
 
+string Stringify(
+    const string& key1,
+    const Range& range1,
+    const string& key2,
+    const Range& range2) {
+  return Stringify(key1, range1) + " " + Stringify(key2, range2);
+}
+
 void DnaDelta::Print(ofstream& out_file) const {
   for (const auto& [key, value] : data_) {
     for (const auto& range : value) {
-      out_file << type_ << " ";
-      out_file << Stringify(key, range) << "\n";
+      out_file << type_ << " " << Stringify(key, range) << "\n";
     }
   }
 }
 
 void DnaDelta::Set(const string& key, const Range& value) {
   data_[key].push_back(value);
+  logger.Debug(
+      "DnaDelta::Set", "Saved: " + type_ + " " + Stringify(key, value));
 }
 
 void DnaMultiDelta::Print(ofstream& out_file) const {
   for (const auto& [key, value] : data_) {
     for (const auto& range : value) {
-      out_file << type_ << " ";
-      out_file << Stringify(key.first, range.first) << " "
-               << Stringify(key.second, range.second) << "\n";
+      out_file << type_ << " "
+               << Stringify(key.first, range.first, key.second, range.second)
+               << "\n";
     }
   }
 }
@@ -46,4 +55,7 @@ void DnaMultiDelta::Set(
     const string& key2,
     const Range& value2) {
   data_[make_pair(key1, key2)].push_back(make_pair(value1, value2));
+  logger.Debug(
+      "DnaDelta::Set",
+      "Saved: " + type_ + " " + Stringify(key1, value1, key2, value2));
 }
