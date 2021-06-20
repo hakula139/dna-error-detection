c0ad846ded179d0d16d7e2265deb46240d061903
Hakula Chen <i@hakula.xyz>
2021-06-20 17:57:19 +0800

    chore: modify parameters for better minimizers finding

--- src/utils/config.h
+++ src/utils/config.h
@@ -21,18 +21,20 @@ struct Config {
   inline static const std::string OVERLAPS_FILENAME = "overlaps.txt";
   inline static const std::string DELTAS_FILENAME = "sv.bed";
 
-  inline static const int LOG_LEVEL = Config::Level::DEBUG;
+  inline static const int LOG_LEVEL = Config::Level::TRACE;
   inline static const size_t HASH_SIZE = 15;
   inline static const size_t WINDOW_SIZE = 10;
   inline static const size_t CHUNK_SIZE = 50000;
-  inline static const size_t SNAKE_MIN_LEN = 30;
-  inline static const size_t ERROR_MAX_LEN = 0;
-  inline static const size_t GAP_MAX_DIFF = 100;
+  inline static const size_t SNAKE_MIN_LEN = 0;
+  inline static const size_t ERROR_MAX_LEN = 10;
+  inline static const size_t GAP_MIN_DIFF = 10;
+  inline static const size_t GAP_MAX_DIFF = 50;
+  inline static const size_t DELTA_MIN_LEN = 100;
   inline static const size_t DELTA_MAX_LEN = 1000;
   inline static const size_t OVERLAP_MIN_LEN = 30;
   inline static const size_t MINIMIZER_MIN_COUNT = 10;
-  inline static const size_t MINIMIZER_MIN_LEN = 150;
-  inline static const int DP_PENALTY = 1;
+  inline static const size_t MINIMIZER_MIN_LEN = 70;
+  inline static const int DP_PENALTY = 2;
   inline static const double STRICT_EQUAL_RATE = 0.5;
   inline static const double FUZZY_EQUAL_RATE = 0.8;
 };
